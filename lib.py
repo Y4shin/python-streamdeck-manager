3df279552fa8d0ad8945e3d1de3f464e0d8bd1b4
Patric Plattner <patric@plattner.com.de>
2020-05-28 03:04:40 +0200

    Some refactoring of lib.py.

--- lib.py
+++ lib.py
@@ -1,3 +1,4 @@
+import importlib.util
 import os
 import threading
 import logging
@@ -73,7 +74,7 @@ class Key:
     self.name           = name
     self.icon_pressed   = icon_pressed
     self.icon_released  = icon_released
-    self.label_pressed  = icon_pressed
+    self.label_pressed  = label_pressed
     self.label_released = label_released
     self.logger         = logging.getLogger(logger_id)
     self.logger_id      = logger_id
@@ -81,7 +82,7 @@ class Key:
     self.on_press_fun   = on_press
     self.pressed        = False
     self.logger.info("Key {} created.".format(str(self)))
-    delf.logger.debug(json.dumps({
+    self.logger.debug(json.dumps({
         "name": self.name,
         "icon": {
           "pressed": self.icon_pressed,
@@ -93,6 +94,22 @@ class Key:
         },
         "logger": self.logger_id
       }))
+
+  def dump(self):
+    return {
+      "name": self.name,
+      "label": {
+        "pressed":  self.label_pressed,
+        "released": self.label_released
+      },
+      "icon": {
+        "pressed":  self.icon_pressed,
+        "released": self.icon_released
+      },
+      "logger": self.logger_id,
+      "pressed": self.pressed,
+      "state": self.state
+    }
 
   def on_press(self, page, deck_man):
     """
@@ -120,19 +137,25 @@ class Page:
 
   def __str__(self):
     keys = list(map(lambda x: str(x), self.deck_keys))
-    return json.dumps({"name": self.__name, "type": self.__deck_type, "data": keys})
+    return json.dumps({"name": self.name, "data": keys})
 
   def get_key_style(self, key):
     """
     Returns styling information for given key.
     """
     key = self.deck_keys[key]
-
-    return {
-      "name":  key.name,
-      "icon":  key.icon_pressed  if key.pressed else key.icon_released,
-      "label": key.label_pressed if key.pressed else key.pressed_released
-    }
+    if key is None:
+      return {
+        "name":  None,
+        "icon":  None,
+        "label": None
+      }
+    else:
+      return {
+        "name":  key.name,
+        "icon":  key.icon_pressed  if key.pressed else key.icon_released,
+        "label": key.label_pressed if key.pressed else key.label_released
+      }
 
   def on_press(self, key, deck_man):
     """
@@ -160,6 +183,8 @@ class Manager:
         self.page_stash[self.current_page].deck_keys[key].name,
         self.page_stash[self.current_page].name,
         ("has been pressed" if state else "has been released")))
+      self.logger.debug("Key = {}".format(
+        json.dumps(self.page_stash[self.current_page].deck_keys[key].dump())))
       self.page_stash[self.current_page].deck_keys[key].pressed = state
       self.page_stash[self.current_page].on_press(key, self)
       self.update_keys()
@@ -171,7 +196,7 @@ class Manager:
     """
     dimensions = self.deck.key_layout()
     n_keys = dimensions[0] * dimensions[1]
-    self.__logger.debug("Updating keys")
+    self.logger.debug("Updating keys")
     for key in list(range(0, n_keys)):
       self.__update_key_image(key)
 
@@ -185,14 +210,18 @@ class Manager:
     if key < n_keys:
       if self.page_stash[self.current_page].deck_keys[key] is not None:
         key_style = self.page_stash[self.current_page].get_key_style(key)
-        if os.path.isfile(key_style["icon"]):
-          image = self.__render_key_image(key_style["icon"],
+        icon_path = os.path.join(self.config_path, key_style["icon"])
+        if os.path.isfile(icon_path):
+          image = self.__render_key_image(icon_path,
                                           key_style["label"])
-          deck.set_key_image(key, image)
+          self.deck.set_key_image(key, image)
         else:
           self.logger.critical("Image {} does not exist!, aborting!"
             .format(key_style["icon"]))
-          raise IOError("Image {} does not exist!.".format(key_style["icon"]))
+          raise IOError("Image {} does not exist!.".format(icon_path))
+      else:
+        image = self.__render_key_image(None, None)
+        self.deck.set_key_image(key, image)
     else:
       raise ValueError("Key dimensions {} outside of board {}."
         .format((key // dimensions[1], key % dimensions[0]), dimensions))
@@ -207,7 +236,7 @@ class Manager:
     image = PILHelper.create_image(self.deck)
 
     if icon_file is not None:
-      icon = Image.open(icon_file).convert('RGBA')
+      icon = Image.open(os.path.join(self.config_path, icon_file)).convert('RGBA')
       icon.thumbnail((image.width, image.height - 20), Image.LANCZOS)
       icon_pos = ((image.width - icon.width) // 2, 0)
       image.paste(icon, icon_pos, icon)
@@ -220,20 +249,17 @@ class Manager:
       label_pos = ((image.width - label_w) // 2, image.height - 20)
       draw.text(label_pos, text=label_text, font=font, fill='white')
 
-    return PILHelper.to_native_format(deck, image)
+    return PILHelper.to_native_format(self.deck, image)
 
 
 
-  def __init__(self, deck, font=STANDARD_FONT, font_size=STANDARD_FONT_SIZE, logger=STANDARD_LOGGER_ID, brightness=STANDARD_BRIGHTNESS):
+  def __init__(self, deck, font=STANDARD_FONT, font_size=STANDARD_FONT_SIZE, logger=STANDARD_LOGGER_ID, brightness=STANDARD_BRIGHTNESS, config_path=os.path.join(os.environ.get('XDG_CONFIG_HOME'), 'streamdeck_manager')):
+    self.config_path  = config_path
     self.deck         = deck
     self.font         = font
     self.logger_id    = logger
     self.logger       = logging.getLogger(self.logger_id)
-    log_formatter     = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
-    console_handler   = logging.StreamHandler()
-    console_handler.setFormatter(log_formatter)
-    self.logger.addHandler(console_handler)
-    self.page_stash   = {"main": StreamDeckPage("Default Page", deck_type=self.__type, logger=self.__logger_id)}
+    self.page_stash   = {"main": Page("Default Page", deck.key_layout(), logger_id=self.logger_id)}
     self.current_page = "main"
     self.brightness   = brightness
     self.font_size    = font_size
